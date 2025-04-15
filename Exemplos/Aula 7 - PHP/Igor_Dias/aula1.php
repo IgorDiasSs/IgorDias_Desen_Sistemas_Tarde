4c9c930fb7a36b9eb5470597eddc6a983f54786c
IgorDias <igordasilvadias07@gmail.com>
2025-04-15 15:34:40 -0300

    Começando PHP

---
+++ Exemplos/Aula 7 - PHP/Igor_Dias/aula1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro Programa PHP</title>
+</head>
+<body>
+    <?php
+        echo "<h1>Hello World, PHP-8</h1>";
+        // phpinfo();
+        echo "Igor da Silva Dias<br><hr>";
+    ?>
+
+    <?php
+        print "teste </br>";
+        print "Olá Mundo</br>";
+        print "Escape 'chars' são os Mesmos com em C\n</br>";
+        print "Você pode ter quebras de linhas em uma String.</br> ";
+        print 'Uma String pode usar "aspas". Isso é muito legal!</br>';
+        print "Ainda pode-se usar aspas simples dessa forma It\'s cool!'</br>";
+        
+    ?>
+
+
+    <?php
+        echo "<h2 align='center'>
+              O meu programa está ecoando corretamente no meu servidor PHP
+              </h2>";
+    ?>
+</body>
+</html>
---
+++ Exemplos/Aula 7 - PHP/Igor_Dias/aula2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Combinação com HTML</title>
+</head>
+<body>
+    <h3>Igor da Silva Dias</h3><br>
+    <?php
+        // Função Usada para definir fuso horário padrão
+        date_default_timezone_set("America/Sao_Paulo");
+        //Manipulando HTML e PHP
+        $data_hoje = date("d/m/Y-h:i:sa");
+    ?>
+    <p align="center">Hoje é Dia <?php echo $data_hoje; ?></p>
+</body>
+</html>
